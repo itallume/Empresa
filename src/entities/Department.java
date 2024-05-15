f3349f2979ed636bbeb1d19476e5d7a581547091
Itallume <itallo.oliver21@gmail.com>
2024-05-15 07:56:54 -0300

    classe program finalizada

---
+++ src/entities/Department.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Department {
+    public Department(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+}
